c92a9ab892fe68fdf806da0e21ba81707f37795c
Pavit Pimchanagul <pavitpim@gmail.com>
2023-04-10 18:40:56 +0700

    MANDATORY : All test pass, some case still warning

--- final/mandatory/pipex.c
+++ final/mandatory/pipex.c
@@ -6,7 +6,7 @@
 /*   By: ppimchan <ppimchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 16:45:10 by ppimchan          #+#    #+#             */
-/*   Updated: 2023/04/10 18:25:01 by ppimchan         ###   ########.fr       */
+/*   Updated: 2023/04/10 18:39:27 by ppimchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static char	*find_path(char **envp)
 	return (*envp + 5);
 }
 
-static char	get_execute_path(char **paths, char *cmd)
+static char	*get_execute_path(char **paths, char *cmd)
 {
 	char	*tmp;
 	char	*command;
@@ -37,53 +37,62 @@ static char	get_execute_path(char **paths, char *cmd)
 	return (NULL);
 }
 
+static void	first_child_process(t_pipe t, char **argv, char **envp)
+{
+	close(t.pipe_fd[0]);
+	dup2(t.infile,STDIN_FILENO);
+	dup2(t.pipe_fd[1],STDOUT_FILENO);
+
+	t.cmd_args = ft_split(argv[2], ' ');
+	t.execute_path =get_execute_path(t.env_path_lists ,t.cmd_args[0]);
+	execve(t.execute_path, t.cmd_args  , envp);
+
+	// NEED TO EXIT ?
+	close(t.pipe_fd[1]);
+	exit(EXIT_SUCCESS);
+}
+
+
+static void	second_child_process(t_pipe t, char **argv, char **envp)
+{
+	close(t.pipe_fd[1]);
+	dup2(t.pipe_fd[0],STDIN_FILENO);
+	dup2(t.outfile,STDOUT_FILENO);
+
+	t.cmd_args = ft_split(argv[3], ' ');
+	t.execute_path =get_execute_path(t.env_path_lists ,t.cmd_args[0]);
+	execve(t.execute_path, t.cmd_args  , envp);
+
+	// NEED TO EXIT ?
+	close(t.pipe_fd[0]);
+	exit(EXIT_SUCCESS);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
-	t_pipe	pipex;
+	t_pipe	t;
 
 	// OPEN INFILE,OUTFILE
-	pipex.infile = open(argv[1],O_RDONLY);
-	pipex.outfile  = open(argv[argc - 1], O_TRUNC | O_CREAT | O_RDWR, 0000644);
-	pipex.env_path = find_path(envp);
-	pipex.env_path_lists = ft_split(pipex.env_path,':');
+	t.infile = open(argv[1],O_RDONLY);
+	t.outfile  = open(argv[argc - 1], O_TRUNC | O_CREAT | O_RDWR, 0000644);
+	t.env_path = find_path(envp);
+	t.env_path_lists = ft_split(t.env_path,':');
 
 	// OPEN NEW PIPE
-	if(pipe(pipex.pipe_fd) == -1)
+	if(pipe(t.pipe_fd) == -1)
 	{
 		perror("pipe");
 		exit(EXIT_FAILURE);
 	}
-	pipex.pid_first_child = fork();
-	if (pipex.pid_first_child == 0) 
-	{
-		close(pipex.pipe_fd[0]);
-		dup2(pipex.infile,STDIN_FILENO);
-		dup2(pipex.pipe_fd[1],STDOUT_FILENO);
+	t.pid_first_child = fork();
+	if (t.pid_first_child == 0) 
+		first_child_process(t,argv,envp);
 
-		pipex.cmd_args = ft_split(argv[2], ' ');
-		pipex.execute_path =get_execute_path(pipex.env_path_lists ,pipex.cmd_args[0]);
-		execve(pipex.execute_path, pipex.cmd_args  , NULL);
+	t.pid_second_child = fork();
+	if(t.pid_second_child == 0)
+		second_child_process(t,argv,envp);
 
-		close(pipex.pipe_fd[1]);
-		exit(EXIT_SUCCESS);
-	} 
-
-	pipex.pid_second_child = fork();
-	if(pipex.pid_second_child == 0) {
-
-		close(pipex.pipe_fd[1]);
-		dup2(pipex.pipe_fd[0],STDIN_FILENO);
-		dup2(pipex.outfile,STDOUT_FILENO);
-
-		pipex.cmd_args = ft_split(argv[3], ' ');
-		pipex.execute_path =get_execute_path(pipex.env_path_lists ,pipex.cmd_args[0]);
-		execve(pipex.execute_path, pipex.cmd_args  , NULL);
-
-		close(pipex.pipe_fd[0]);
-		exit(EXIT_SUCCESS);
-	}
-	waitpid(pipex.pid_first_child, NULL,0);
-	waitpid(pipex.pid_second_child, NULL,0);
+	waitpid(t.pid_first_child, NULL,0);
+	waitpid(t.pid_second_child, NULL,0);
 	return(0);
 }
